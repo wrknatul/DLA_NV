18e5e52413065388cd956c0cfb2b4cac69d48fd9
wrknatul <wrk.natul@yandex.ru>
2024-01-31 03:50:09 +0300

    Modified test and README files.

--- loader.sh
+++ loader.sh
@@ -1,5 +1,6 @@
 printf "downloading LjSpeech...\n"
-# axel -n 8 https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2 -q || printf "Failed to load ljspeech\n"
+wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
+
 mkdir data
 tar -xvf LJSpeech-1.1.tar.bz2 >> /dev/null
 mv LJSpeech-1.1 data/LJSpeech-1.1
--- test.py
+++ test.py
@@ -1,34 +1,36 @@
 import argparse
+import multiprocessing
+from collections import defaultdict
 import json
 import os
 from pathlib import Path
 
+import numpy as np
+import soundfile as sf
 import torch
+import torchaudio
 from tqdm import tqdm
 
+from hw_nv.datasets import MelSpectrogram
+from hw_nv.logger import WanDBWriter
 import hw_nv.model as module_model
-from hw_nv.trainer import Trainer
 from hw_nv.utils import ROOT_PATH
-from hw_nv.utils.object_loading import get_dataloaders
 from hw_nv.utils.parse_config import ConfigParser
+from train import SEED
 
-DEFAULT_CHECKPOINT_PATH = ROOT_PATH / "default_test_model" / "checkpoint.pth"
+DEFAULT_CHECKPOINT_PATH = ROOT_PATH / "best_model" / "checkpoint.pth"
+torch.manual_seed(SEED)
 
 
-def main(config, out_file):
+def main(args, config):
     logger = config.get_logger("test")
+    writer: WanDBWriter = WanDBWriter(config, logger) if args.log_wandb else None
 
     # define cpu or gpu if possible
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # text_encoder
-    text_encoder = config.get_text_encoder()
-
-    # setup data_loader instances
-    dataloaders = get_dataloaders(config, text_encoder)
-
     # build model architecture
-    model = config.init_obj(config["arch"], module_model, n_class=len(text_encoder))
+    model = config.init_obj(config["arch"], module_model)
     logger.info(model)
 
     logger.info("Loading checkpoint: {} ...".format(config.resume))
@@ -42,46 +44,42 @@ def main(config, out_file):
     model = model.to(device)
     model.eval()
 
-    results = []
+    # prepare mel-spec
+    mel_spec = MelSpectrogram(config["mel_spec"])
+    mel_spec = mel_spec.to(device)
 
+    # read audio filenames and texts
+    audio_names = [filename for filename in sorted(Path(args.input_dir).iterdir()) if filename.suffix == ".wav"]
+    text_path = Path(args.input_dir) / "text.txt"
+    if text_path.exists():
+        with open(text_path) as file:
+            texts = [line.strip() for line in file.readlines()]
+
+    sr = config["preprocessing"]["sr"]
     with torch.no_grad():
-        for batch_num, batch in enumerate(tqdm(dataloaders["test"])):
-            batch = Trainer.move_batch_to_device(batch, device)
-            output = model(**batch)
-            if type(output) is dict:
-                batch.update(output)
-            else:
-                batch["logits"] = output
-            batch["log_probs"] = torch.log_softmax(batch["logits"], dim=-1)
-            batch["log_probs_length"] = model.transform_input_lengths(
-                batch["spectrogram_length"]
-            )
-            batch["probs"] = batch["log_probs"].exp().cpu()
-            batch["argmax"] = batch["probs"].argmax(-1)
-            for i in range(len(batch["text"])):
-                argmax = batch["argmax"][i]
-                argmax = argmax[: int(batch["log_probs_length"][i])]
-                results.append(
-                    {
-                        "ground_trurh": batch["text"][i],
-                        "pred_text_argmax": text_encoder.ctc_decode(argmax.cpu().numpy()),
-                        "pred_text_beam_search": text_encoder.ctc_beam_search(
-                            batch["probs"][i], batch["log_probs_length"][i], beam_size=100
-                        )[:10],
-                    }
-                )
-    with Path(out_file).open("w") as f:
-        json.dump(results, f, indent=2)
+        for idx, audio_name in tqdm(enumerate(audio_names), total=len(audio_names), desc="Infer"):
+            name = audio_name.stem
+            audio, _ = sf.read(os.path.join('', audio_name))
+            audio = torch.tensor(audio).to(torch.float32).unsqueeze(0).to(device)
+            spec = mel_spec(audio)
+            pred_audio = model.generator(spec).squeeze(1)
+            torchaudio.save(Path(args.output) / f"{name}.wav", pred_audio, sample_rate=sr)
+            if writer is not None:
+                writer.set_step(step=idx, mode="test")
+                if text_path.exists():
+                    writer.add_text("text", texts[idx])
+                writer.add_audio("pred_audio", pred_audio, sample_rate=sr)
+                writer.add_audio("target_audio", audio, sample_rate=sr)
 
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser(description="PyTorch Template")
     args.add_argument(
-        "-c",
-        "--config",
-        default=None,
+        "-i",
+        "--input-dir",
         type=str,
-        help="config file path (default: None)",
+        default=str(DEFAULT_CHECKPOINT_PATH.parents[1] / "test_data"),
+        help="Path to file with texts and audios to test"
     )
     args.add_argument(
         "-r",
@@ -100,30 +98,16 @@ if __name__ == "__main__":
     args.add_argument(
         "-o",
         "--output",
-        default="output.json",
+        default="output",
         type=str,
-        help="File to write results (.json)",
+        help="Dir to write result audio",
     )
     args.add_argument(
-        "-t",
-        "--test-data-folder",
-        default=None,
-        type=str,
-        help="Path to dataset",
-    )
-    args.add_argument(
-        "-b",
-        "--batch-size",
-        default=20,
-        type=int,
-        help="Test dataset batch size",
-    )
-    args.add_argument(
-        "-j",
-        "--jobs",
-        default=1,
-        type=int,
-        help="Number of workers for test dataloader",
+        "-l",
+        "--log-wandb",
+        default=False,
+        type=bool,
+        help="Save results in wandb or not (wand params are in config file)"
     )
 
     args = args.parse_args()
@@ -138,35 +122,7 @@ if __name__ == "__main__":
     with model_config.open() as f:
         config = ConfigParser(json.load(f), resume=args.resume)
 
-    # update with addition configs from `args.config` if provided
-    if args.config is not None:
-        with Path(args.config).open() as f:
-            config.config.update(json.load(f))
+    # prepare output dir
+    Path(args.output).mkdir(exist_ok=True, parents=True)
 
-    # if `--test-data-folder` was provided, set it as a default test set
-    if args.test_data_folder is not None:
-        test_data_folder = Path(args.test_data_folder).absolute().resolve()
-        assert test_data_folder.exists()
-        config.config["data"] = {
-            "test": {
-                "batch_size": args.batch_size,
-                "num_workers": args.jobs,
-                "datasets": [
-                    {
-                        "type": "CustomDirAudioDataset",
-                        "args": {
-                            "audio_dir": str(test_data_folder / "audio"),
-                            "transcription_dir": str(
-                                test_data_folder / "transcriptions"
-                            ),
-                        },
-                    }
-                ],
-            }
-        }
-
-    assert config.config.get("data", {}).get("test", None) is not None
-    config["data"]["test"]["batch_size"] = args.batch_size
-    config["data"]["test"]["n_jobs"] = args.jobs
-
-    main(config, args.output)
+    main(args, config)
